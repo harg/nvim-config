66c712d5a2a6e9b4cc7d965c35afd970329599d1
harg <harold.gay@gmail.com>
2024-05-18 00:17:43 +0200

    set timeoutlen

--- lua/harg/core/options.lua
+++ lua/harg/core/options.lua
@@ -5,6 +5,9 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
 
+opt.timeoutlen = 500
+opt.ttimeoutlen = 10
+
 -- tabs & indentation
 opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 4 spaces for indent width
